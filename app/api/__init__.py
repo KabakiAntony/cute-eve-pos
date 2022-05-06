c034be8cc2e9d46e04a2ce130b83312aa6ae26b0
KabakiAntony <kabaki.antony@gmail.com>
2022-05-06 10:51:34 +0300

    created various blueprints

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,1 +1,6 @@
-# define blueprints here.
+from flask import Blueprint
+
+
+users = Blueprint("users", __name__, url_prefix="")
+stocks = Blueprint("stocks", __name__, url_prefix="")
+sales = Blueprint("sales", __name__, url_prefix="")
