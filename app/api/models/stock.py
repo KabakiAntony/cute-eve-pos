ec73b33c70c2a49c033d5415510c477937c006b2
KabakiAntony <kabaki.antony@gmail.com>
2022-05-13 18:52:27 +0300

    refined the model definition

--- app/api/models/stock.py
+++ app/api/models/stock.py
@@ -8,13 +8,15 @@ class Stock(db.Model):
     __tablename__ = "stock"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     item_sys_id = db.Column(db.String(25), unique=True, nullable=False)
+    action_id = db.Column(db.String(25), db.ForeignKey('Action.action_sys_id'))
     item = db.Column(db.String(255), unique=True, nullable=False)
     quantity = db.Column(db.Numeric, nullable=False)
     buying_price = db.Column(db.Float, nullable=False)
     selling_price = db.Column(db.Float, nullable=False)
 
-    def __init__(self, item_syd, item, qty, bp, sp):
+    def __init__(self, item_syd, action_id, item, qty, bp, sp):
         self.item_sys_id = item_syd
+        self.action_id = action_id
         self.item = item
         self.quantity = qty
         self.buying_price = bp
@@ -24,7 +26,8 @@ class Stock(db.Model):
 class StockSchema(ma.Schema):
     class Meta:
         fields = (
-            "item_sys_id" "item", "quantity", "buying_price" "selling_price")
+            "item_sys_id", "action_id", "item", "quantity", 
+            "buying_price" "selling_price")
 
 
 stock_schema = StockSchema()
