201b71b56158ebaed801c16cef50ede114f25a37
KabakiAntony <kabaki.antony@gmail.com>
2022-05-16 21:09:04 +0300

    configured the app for production and registered blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,29 @@
 from flask import Flask
+from config import ProductionConfig
+from flask_migrate import Migrate
+from app.api.views.sale import sales as sales_blueprint
+from app.api.views.stock import stocks as stocks_blueprint
+from app.api.views.user import users as users_blueprint
+from flask_cors import CORS
+
+
+migrate = Migrate(compare_type=True)
 
 
 def create_app():
     """ initializing the app factory """
     app = Flask(__name__)
+    CORS(app)
+    app.config.from_object(ProductionConfig())
+
+    from app.api.models import db, ma
+
+    db.init_app(app)
+    ma.init_app(app)
+    migrate.init_app(app, db)
+    app.register_blueprint(sales_blueprint)
+    app.register_blueprint(stocks_blueprint)
+    app.register_blueprint(users_blueprint)
+    app.app_context().push()
 
     return app
