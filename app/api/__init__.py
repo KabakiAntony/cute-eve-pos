70710e4365b115d27e7347ae2462fde40308fe99
KabakiAntony <kabaki.antony@gmail.com>
2022-05-17 16:15:51 +0300

    created an items blueprint

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,5 +2,5 @@ from flask import Blueprint
 
 
 users = Blueprint("users", __name__, url_prefix="")
-stocks = Blueprint("stocks", __name__, url_prefix="")
+items = Blueprint("items", __name__, url_prefix="")
 sales = Blueprint("sales", __name__, url_prefix="")
