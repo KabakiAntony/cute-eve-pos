e8cb87ff83ef158226d24520cfc2dab948d39a8a
KabakiAntony <kabaki.antony@gmail.com>
2022-08-01 13:49:41 +0300

    created a new sales table to accomodate getting of composite item information

--- app/api/models/sale.py
+++ app/api/models/sale.py
@@ -2,20 +2,20 @@ from app.api.models import db, ma
 
 
 class Sale(db.Model):
-    """sales model"""
-    __tablename__ = "Sale"
+    """better sales model"""
+    __tablename__ = "Sale_Two"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     sale_id = db.Column(
         db.String(25),  db.ForeignKey('Action.action_sys_id'))
     item_id = db.Column(db.String(25), db.ForeignKey('Item.item_sys_id'))
-    unit_price = db.Column(db.Float, nullable=False)
+    buying_price = db.Column(db.Float, nullable=False)
     units = db.Column(db.Float, nullable=False)
     total = db.Column(db.Float, nullable=False)
     
-    def __init__(self, sale_id, item_id, unit_price, units, total):
+    def __init__(self, sale_id, item_id, buying_price, units, total):
         self.sale_id = sale_id
         self.item_id = item_id
-        self.unit_price = unit_price
+        self.buying_price = buying_price
         self.units = units
         self.total = total
 
@@ -24,7 +24,7 @@ class SaleSchema(ma.Schema):
     class Meta:
         fields = (
             "item_id", "sale_id",
-            "unit_price", "units", "total"
+            "buying_price", "units", "total"
             )
 
 
